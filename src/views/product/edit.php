727595bf71cbef4f7139721b947065abb61aba6d
Rubens-Lira <rubenslira1156@gmail.com>
2025-02-27 09:48:44 -0300

    Product

---
+++ src/views/product/edit.php
@@ -0,0 +1,53 @@
+<div class="container">
+    <a href="index.php" class="voltar">Voltar</a>
+
+    <div class="user-info">
+        <img src="./src/assets/img/usuario.jp" alt="Usuário" class="user-img">
+        <span class="user">Usuário</span>
+    </div>
+
+    <h2>Comece a empreender</h2>
+    <p>Cadastre os seus produtos</p>
+
+    <form method="post">
+        <label for="nome">Nome do Produto:</label>
+        <input 
+            type="text" 
+            name="name" 
+            id="nome" placeholder="Digite o nome do produto"
+            value="<?= $products['pdt_name'] ?? '' ?>"
+        />
+
+        <label for="valor">Valor:</label>
+        <input 
+            type="number" 
+            name="price" 
+            id="valor" step="0.01" min="0"  placeholder="Digite valor do produto"
+            value="<?= $products['pdt_unit_price'] ?? 0 ?>"
+        />
+
+        <label for="quantidade">Quantidade:</label>
+        <input 
+            type="text" 
+            name="units" 
+            id="quantidade" 
+            placeholder="Digite a quantidade do produto"
+            value="<?= $products['pdt_units'] ?? 0 ?>"
+        />
+
+        <label for="description">Description:</label>
+        <textarea name="description" id="description">
+        <?= $products['pdt_description'] ?? '' ?>
+        </textarea>
+        <!-- <label for="imagem">Upload de Imagem:</label>
+        <input type="file" id="imagem"> -->
+
+        <button type="submit">Atualizar</button>
+    </form>
+    <div class="erros">
+        <?php if (isset($error['query'])): ?>
+            <span class="erro"><?= htmlspecialchars($error['query']) ?></span>
+            <?php $error = [] ?>
+        <?php endif ?>
+    </div>
+</div>
