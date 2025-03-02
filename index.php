b79ced430e0a3c97e8613061556f869018a755a1
Rubens-Lira <rubenslira1156@gmail.com>
2025-03-02 09:30:57 -0300

    docs: Explicaçãos das rotas

--- index.php
+++ index.php
@@ -19,18 +19,18 @@ $offset = isset($_GET['offset']) && is_numeric($_GET['offset']) && $_GET['offset
     : 0;
     
 $result = match ($action) {
-    'home' => ['view' => './src/views/home.php', 'title' => 'Página Inicial', 'css' => './src/assets/styles/index.css'],
+    'home' => ['view' => './src/views/home.php', 'title' => 'Página Inicial', 'css' => './src/assets/styles/index.css'], // Página principal
     'about' => ['view' => './src/views/about.php', 'title' => 'Sobre'],
     "exemplo" => ['view' => './src/views/exemplo.html', 'title' => 'titulo', 'css' => './src/assets/styles/exemplo.css'],
     'login' => $UserController->login(),
-    'user-create' => $UserController->create(),
-    'user-edit' => $UserController->edit(),
-    'product-create' => $ProductController->create(),
-    'product-list' => $ProductController->list($offset),
-    'product-edit' => $ProductController->edit($id),
+    'user-create' => $UserController->create(), // Cria usuários src/views/user/create.php
+    'user-edit' => $UserController->edit(), // Edita usuários src/views/user/edit.php
+    'product-create' => $ProductController->create(), // Cria produtos src/views/product/create.php
+    'product-list' => $ProductController->list($offset), // Lista os produtos do vendedor
+    'product-edit' => $ProductController->edit($id), // Cria edita src/views/product/edit.php
     'product-read' => $ProductController->read($id),
-    'user' => ['view' => './src/views/user/user.php', 'title' => 'Perfil'],
-    'vendas' => $ProductController->listAll($offset),
+    'user' => ['view' => './src/views/user/user.php', 'title' => 'Perfil'], // Perfil do usuário
+    'vendas' => $ProductController->listAll($offset), // Lista os produtos de todos os vendedores
     'logout' => ['view' => './src/config/logout.php', 'title' => 'Saindo'],
     'carrinho' => ['view' => './src/views/product/carrinho.php', 'title' => 'Carrinho'],
     'adicionar_carrinho' => ['view' => './src/views/product/adicionar_carrinho.php', 'title' => 'Carrinho'],
