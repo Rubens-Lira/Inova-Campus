32e2b14100e960c0180ecfc441235dbcb676d5b5
Izabelle Alves <izabelle.alvesbl@gmail.com>
2025-03-09 00:52:10 -0300

    Catarina, Izabelle, Laura e Rubens// Caminho do usuário na parte superior da tela

--- src/views/login.php
+++ src/views/login.php
@@ -3,31 +3,24 @@
         <h1>Inova Campus</h1>
     </header>
     <main>
+        <nav>
+            <a href="index.php">Página Inicial</a>
+            &#62;
+            <a href="index.php?action=login">Login</a>
+        </nav>
         <h2>Faça negócios no Inova Campus</h2>
         <h3 class="subtitulo">Onde vendedores e clientes encontram as melhores oportunidades para comprar e vender</h3>
         <form method="post">
             <label for="email">
                 Email:
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="Digite aqui ..."
-                    value="<?= $_POST['email'] ?? ''?>"
-                    required
-                />
+                <input type="email" id="email" name="email" placeholder="Digite aqui ..."
+                    value="<?= $_POST['email'] ?? '' ?>" required />
             </label>
             <label for="senha">
                 Senha:
-                <input
-                    type="password"
-                    id="senha"
-                    name="password"
-                    placeholder="Digite aqui ..."
-                    required
-                />
+                <input type="password" id="senha" name="password" placeholder="Digite aqui ..." required />
             </label>
-            
+
             <a href="#" class="esqueceu">Esqueceu a senha?</a>
             <button type="submit" class="btn entrar">Entrar</button>
             <a href="index.php?action=user-create" class="btn cadastrar">Não possui conta?</a>
