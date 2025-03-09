da220f2118a6ee03083ebcb1985db83f4de6aeac
Izabelle Alves <izabelle.alvesbl@gmail.com>
2025-03-09 00:46:29 -0300

    Catarina, Izabelle, Laura e Rubens// Ícones de edição removidos

--- src/views/user/edit.php
+++ src/views/user/edit.php
@@ -17,11 +17,7 @@
   <form method="post">
     <label for="email">
       Email:
-      <input
-        type="email"
-        id="email"
-        name="email"
-        placeholder="Digite seu email"
+      <input type="email" id="email" name="email" placeholder="Digite seu email"
         value="<?= htmlspecialchars($_SESSION['user']['email']) ?>" />
       <?php if (!empty($error['email'])): ?>
         <span class="erro"><?= htmlspecialchars($error['email']) ?></span>
@@ -30,11 +26,7 @@
 
     <label for="nome">
       Nome Completo:
-      <input
-        type="text"
-        id="nome"
-        name="name"
-        placeholder="Digite seu nome"
+      <input type="text" id="nome" name="name" placeholder="Digite seu nome"
         value="<?= htmlspecialchars($_SESSION['user']['name']) ?>" />
       <?php if (!empty($error['name'])): ?>
         <span class="erro"><?= htmlspecialchars($error['name']) ?></span>
@@ -43,16 +35,20 @@
 
     <label for="whatsapp">
       Número do WhatsApp:
-      <input
-        type="tel"
-        id="whatsapp"
-        name="tel"
-        placeholder="Digite apenas números"
+      <input type="tel" id="whatsapp" name="tel" placeholder="Digite apenas números"
         value="<?= htmlspecialchars($_SESSION['user']['tel']) ?>" />
       <?php if (!empty($error['tel'])): ?>
         <span class="erro"><?= htmlspecialchars($error['tel']) ?></span>
       <?php endif ?>
     </label>
+
+    <p>Eu sou:</p>
+    <div class="radio-group">
+      <input type="radio" id="cliente" name="function" value="cliente" checked>
+      <label for="cliente">Cliente</label>
+      <input type="radio" id="vendedor" name="function" value="vendedor">
+      <label for="vendedor">Vendedor</label>
+    </div>
 
     <div class="buttons">
       <button type="reset" class="btn cancel">Descartar Alterações</button>
